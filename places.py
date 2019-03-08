f9ed4858175b0f430d5b7d5df1bc13efb50d2659
Harold Roberson <hdroberson@gmail.com>
2019-03-07 19:04:50 -0600

    Add files via upload

---
+++ places.py
@@ -0,0 +1,7 @@
+places = ['Germany', 'Africa', 'United States', 'Canada']
+for x in places:
+    print(x)
+
+cars = ['Ford', 'Chervolet', 'Pontiac', 'Toyota']
+if "Toyota" in cars:
+    print("I drive a Toyota.")
